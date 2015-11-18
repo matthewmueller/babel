187aa0a73f405e014f1dd012b65d1a2e37bbfc49
Dominic Barnes <dominic@dbarnes.info>
2015-11-17 21:38:59 -0800

    ignoring node_modules by default

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 
 'use strict';
 
-let compile = require('babel-core').transform;
+let core = require('babel-core');
 let debug = require('debug')('mako-babel');
 let defaults = require('defaults');
 
+let compile = core.transform;
+let shouldIgnore = core.util.shouldIgnore;
+
+
 module.exports = function (options) {
-  let config = defaults(options, { extensions: 'js' });
-  debug('initialize %j', config);
+  debug('initialize %j', options);
+
+  let config = defaults(options, {
+    extensions: 'js',
+    ignore: [ /node_modules/i ],
+    only: []
+  });
 
   return function (mako) {
     mako.postread(config.extensions, function babel(file) {
+      if (shouldIgnore(file.path, config.ignore, config.only)) {
+        return debug('ignoring path %s', file.path);
+      }
+
       debug('compiling %s', file.path);
-      let results = compile(file.contents, { filename: file.path });
+      let results = compile(file.contents, {
+        filename: file.path,
+        sourceMaps: 'inline'
+      });
       file.contents = results.code;
-      // TODO: sourcemaps
     });
   };
 };
